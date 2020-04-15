1ad9ab265ed218a296102fe6ae874e9e1e4c8c55
toly <toly.sukhotin@gmail.com>
2020-04-15 16:20:59 +0300

    first commit

---
+++ source_code/module/database.py
@@ -0,0 +1,83 @@
+'''
+Created on Jan 10, 2017
+
+@author: hanif
+'''
+
+import pymysql
+import os
+
+db_host = os.environ['db_host']
+db_username = os.environ['db_username']
+db_password = os.environ['db_password']
+db_name = os.environ['db_name']
+
+class Database:
+    def connect(self):
+        return pymysql.connect(db_host, db_username, db_password, db_name)
+
+    def read(self, id):
+        con = Database.connect(self)
+        cursor = con.cursor()
+
+        try:
+            if id == None:
+                cursor.execute("SELECT * FROM phone_book order by name asc")
+            else:
+                cursor.execute("SELECT * FROM phone_book where id = %s order by name asc", (id,))
+
+            return cursor.fetchall()
+        except:
+            return ()
+        finally:
+            con.close()
+
+    def insert(self, data):
+        con = Database.connect(self)
+        cursor = con.cursor()
+
+        try:
+            cursor.execute("INSERT INTO phone_book(name,phone,address) VALUES(%s, %s, %s)",
+                           (data['name'], data['phone'], data['address'],))
+            con.commit()
+
+            return True
+        except:
+            con.rollback()
+
+            return False
+        finally:
+            con.close()
+
+    def update(self, id, data):
+        con = Database.connect(self)
+        cursor = con.cursor()
+
+        try:
+            cursor.execute("UPDATE phone_book set name = %s, phone = %s, address = %s where id = %s",
+                           (data['name'], data['phone'], data['address'], id,))
+            con.commit()
+
+            return True
+        except:
+            con.rollback()
+
+            return False
+        finally:
+            con.close()
+
+    def delete(self, id):
+        con = Database.connect(self)
+        cursor = con.cursor()
+
+        try:
+            cursor.execute("DELETE FROM phone_book where id = %s", (id,))
+            con.commit()
+
+            return True
+        except:
+            con.rollback()
+
+            return False
+        finally:
+            con.close()
