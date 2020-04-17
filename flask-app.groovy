d57e611d89b5169e797685f8f57c87e6c89d2a0e
toly <toly.sukhotin@gmail.com>
2020-04-17 13:40:39 +0300

    flask-app

--- flask-app.groovy
+++ flask-app.groovy
@@ -1,9 +1,9 @@
 def docker_registry = "sukhotin/flask-crud-app"
 def docker_registry_creds = "dockerhub"
 def docker_image = ""
-def mysql_remote_ip = ""
 def app_port = "8181"
 def mysql_service_name = "mysql.service.consul"
+def db_host = ""
 def db_username = "app"
 def db_password = "admin"
 def db_name = "crud_flask"
@@ -18,7 +18,7 @@ pipeline {
             {
                script
                {
-                   mysql_remote_ip = sh(returnStdout: true, script: "dig +short ${ mysql_service_name }").trim()
+                   db_host = sh(returnStdout: true, script: "dig +short ${ mysql_service_name }").trim()
                }
             }
         }
@@ -37,7 +37,7 @@ pipeline {
             {
                 script 
                 {
-                    with_run_params = "-e db_host=${mysql_remote_ip} -e db_username=${db_username} -e db_password=${db_password} -e db_name=${db_name} -p ${app_port}:${app_port}"
+                    with_run_params = "-e db_host=${ db_host } -e db_username=${ db_username } -e db_password=${ db_password } -e db_name=${ db_name } -p ${ app_port }:${ app_port }"
                     docker.image(docker_registry).withRun(with_run_params) {c ->
                         sh "sleep 5"
                         sh "curl -sf -o /dev/null http://localhost:${app_port}"
--- mysql.groovy
+++ mysql.groovy
@@ -1,6 +1,6 @@
 def mysql_image = "mysql:latest"
 def mysql_service_name = "mysql.service.consul"
-def mysql_remote_ip = ""
+def db_host = ""
 def db_username = "app"
 def db_password = "admin"
 def db_name = "crud_flask"
@@ -15,7 +15,7 @@ pipeline
             {
                script
                {
-                   mysql_remote_ip = sh(returnStdout: true, script: "dig +short ${ mysql_service_name }").trim()
+                   db_host = sh(returnStdout: true, script: "dig +short ${ mysql_service_name }").trim()
                }
             }
         }
@@ -27,7 +27,7 @@ pipeline
             }
             steps 
             {
-                sh "mysql -h ${ mysql_remote_ip } -u${db_username} -p${db_password} -e 'create database ${db_name};'"
+                sh "mysql -h ${ db_host } -u${ db_username } -p${ db_password } -e 'create database ${ db_name };'"
             }
         }
         stage('Import data to new database')
@@ -38,7 +38,7 @@ pipeline
             }
             steps
             {
-                sh "mysql -h ${ mysql_remote_ip } -u${db_user} -p${db_user_pass} ${db_name} < database/${db_name}.sql"
+                sh "mysql -h ${ db_host } -u${ db_username } -p${ db_password } ${ db_name } < database/${db_name}.sql"
             }
         }
     }
