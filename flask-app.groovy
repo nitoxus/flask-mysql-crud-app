0b32ef0769e129f5bb15f1aaa19db05817da4600
toly <toly.sukhotin@gmail.com>
2020-04-17 11:52:38 +0300

    flask-app

--- flask-app.groovy
+++ flask-app.groovy
@@ -2,6 +2,11 @@ def docker_registry = "sukhotin/flask-crud-app"
 def docker_registry_creds = "dockerhub"
 def docker_image = ""
 def app_port = "8181"
+def db_host = "mysql.service.consul"
+def db_user = "app"
+def db_user_pass = "admin"
+def db_name = "crud_flask"
+def with_run_params = "-e db_host=${db_host} -e db_user=${db_user} -e db_user_pass=${db_user_pass} -e db_name=${db_name} -p ${app_port}:${app_port}"
 pipeline {
     agent any 
     stages {
@@ -20,7 +25,7 @@ pipeline {
             {
                 script 
                 {
-                    docker.image(docker_registry).withRun("-p ${app_port}:${app_port}") {c ->
+                    docker.image(docker_registry).withRun(with_run_params) {c ->
                         sh "curl -sSf http://localhost:${app_port}"
                     }
                 }
