ed772197a403a29b4d228719bde7a6e1243c745e
toly <toly.sukhotin@gmail.com>
2020-04-17 12:29:46 +0300

    flask-app

--- flask-app.groovy
+++ flask-app.groovy
@@ -39,7 +39,7 @@ pipeline {
                 {
                     with_run_params = "-e db_host=${mysql_remote_ip} -e db_username=${db_username} -e db_password=${db_password} -e db_name=${db_name} -p ${app_port}:${app_port}"
                     docker.image(docker_registry).withRun(with_run_params) {c ->
-                        sh "curl -sf -o /dev/null http://localhost:${app_port}"
+                        sh "curl http://localhost:${app_port}"
                     }
                 }
             }
