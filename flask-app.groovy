101820ea257ff1d6dfd3d534a0515c242d5bf424
toly <toly.sukhotin@gmail.com>
2020-04-17 11:13:03 +0300

    flask-app

--- flask-app.groovy
+++ flask-app.groovy
@@ -1,54 +1,28 @@
-def registry = "sukhotin/flask-crud-app"
-def registryCredential = "dockerhub"
+def docker_registry = "sukhotin/flask-crud-app"
+def docker_registry_creds = "dockerhub"
+def docker_image = ""
+def app_port = "8181"
 pipeline {
-    environment {
-        
-        
-        deployment = "project-flask.yml"
-        dockerImage = ""
-        get_dns_name_script = "aws elb describe-load-balancers --region us-east-1 --query 'LoadBalancerDescriptions[*].DNSName' --output text"      
-        dns_name = ""
-    }
     agent any 
     stages {
         stage("Build a docker image") {
-            steps {
-                script {
-                    dockerImage = docker.build(registry)
+            steps 
+            {
+                script 
+                {
+                    docker_image = docker.build(docker_registry)
                 }
             }            
         }
-        stage("Test the image") { 
-            steps {
-                script {
-                    docker.image(registry).withRun('-p 8080:5000') {c ->
-                        sh "until \$(curl --output /dev/null --head --fail http://localhost:8080); do printf 'Wait for responce...'; sleep 5; done"
+        stage("Test the image") 
+        { 
+            steps 
+            {
+                script 
+                {
+                    docker.image(docker_registry).withRun("-p ${app_port}:${app_port}") {c ->
+                        sh "until \$(curl --output /dev/null --head --fail http://localhost:${app_port}); do printf 'Wait for responce...'; sleep 5; done"
                     }
-                }
-            }
-        }
-        stage("Push the image to DockerHub") { 
-            steps {
-                script {
-                    withDockerRegistry(credentialsId: 'dockerhub', url: ''){
-                        dockerImage.push()
-                    }
-                }
-            }
-        }
-        stage("Deploy the app image to K8s") { 
-            steps {
-                script {
-                    sh "kubectl apply -f ${deployment}"
-                }
-            }
-        }
-        stage("Wait for load balancer"){
-            steps {
-                script {
-                   sh "sleep 10"
-                   dns_name = sh(returnStdout: true, script: "${ get_dns_name_script }" )
-                   sh "until \$(curl --output /dev/null --head --fail http://${ dns_name }); do printf 'Wait for responce...'; sleep 20; done"
                 }
             }
         }
